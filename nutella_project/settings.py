050874f501c98c0f442d14cbd870fe5627a4ee35
Thibault231 <salgues231@gmail.com>
2020-05-07 17:22:47 +0200

    V. 12 heroku test3

--- nutella_project/settings.py
+++ nutella_project/settings.py
@@ -27,23 +27,11 @@ SECRET_KEY = os.getenv("SKEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('ENV') == 'PRODUCTION':
     DEBUG = False
-    # Static files settings
-    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
-    # Extra places for collectstatic to find static files.
-    STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
-    )
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
-    
 else:
     DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1',
-                'p8djangonutella.herokuapp.com',
+                'djangonutella.herokuapp.com',
                 ]
 
 
@@ -151,3 +139,21 @@ STATIC_URL = '/static/'
 LOGIN_URL = 'purbeurre:connexion'
 
 INTERNAL_IPS = ['127.0.0.1']
+
+
+if os.environ.get('ENV') == 'PRODUCTION':
+    # Static files settings
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+    # Extra places for collectstatic to find static files.
+    STATICFILES_DIRS = (
+        os.path.join(PROJECT_ROOT, 'static'),
+    )
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
